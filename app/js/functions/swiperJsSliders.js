010c88cc4731de7e888c9966e0c0408d9c7465ce
Protorab <50131464+Protorab@users.noreply.github.com>
2021-08-24 22:11:50 +0300

    some work

--- app/js/functions/swiperJsSliders.js
+++ app/js/functions/swiperJsSliders.js
@@ -7,7 +7,7 @@ const swiperJsSliders = () => {
     },
     parallax: true,
     loop: true,
-    speed: 2000,
+    speed: 2500,
     pagination: {
       el: ".top-slider__pagination",
       type: "bullets",
@@ -17,6 +17,67 @@ const swiperJsSliders = () => {
     },
     a11y: {
       paginationBulletMessage: "Перейти к слайду {{index}}",
+    },
+  });
+  let noveltySlider = new Swiper(".novelty-slider", {
+    slidesPerView: "auto",
+    spaceBetween: 45,
+    speed: 1200,
+    navigation: {
+      nextEl: ".novelty-slider__next",
+      prevEl: ".novelty-slider__prev",
+    },
+
+    breakpoints: {
+      320: {
+        spaceBetween: 15,
+        slidesPerView: 1,
+      },
+      640: {
+        spaceBetween: 20,
+        slidesPerView: "auto",
+      },
+      1024: {
+        spaceBetween: 30,
+      },
+      1280: {
+        spaceBetween: 45,
+      },
+    },
+    a11y: {
+      paginationBulletMessage: "Перейти к слайду {{index}}",
+      prevSlideMessage: "Предыдущий слайд",
+      nextSlideMessage: "Следующий слайд",
+    },
+  });
+  let brandsSlider = new Swiper(".brands-slider", {
+    slidesPerView: 4,
+    spaceBetween: 65,
+    speed: 1200,
+    navigation: {
+      nextEl: ".brands-slider__next",
+      prevEl: ".brands-slider__prev",
+    },
+
+    breakpoints: {
+      320: {
+        spaceBetween: 15,
+        slidesPerColumn: 2,
+      },
+      640: {
+        spaceBetween: 20,
+      },
+      1024: {
+        spaceBetween: 45,
+      },
+      1280: {
+        spaceBetween: 65,
+      },
+    },
+    a11y: {
+      paginationBulletMessage: "Перейти к слайду {{index}}",
+      prevSlideMessage: "Предыдущий слайд",
+      nextSlideMessage: "Следующий слайд",
     },
   });
 };
